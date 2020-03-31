f808d74d650c008fc7799473c656f34416c14eb2
chathurangasandun <wacsk19921002@gmail.com>
2020-03-31 23:51:22 +0530

    categirty - get ok

---
+++ CliveKumara.CarwashManagement.Api/AutoMapperProfiles/AutoMapping.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using CliveKumara.CarwashManagement.Api.Models.Dtos;
+using CliveKumara.CarwashManagement.Api.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CliveKumara.CarwashManagement.Api.AutoMapperProfiles
+{
+    public class AutoMapping: Profile
+    {
+        public AutoMapping()
+        {
+            CreateMap<Category, CategoryDto>().ReverseMap(); // means you want to map from User to UserDTO
+        }
+
+    }
+}
--- CliveKumara.CarwashManagement.Api/Controllers/CategoriesController.cs
+++ CliveKumara.CarwashManagement.Api/Controllers/CategoriesController.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using CliveKumara.CarwashManagement.Api.Models.DatabaseModel;
+using CliveKumara.CarwashManagement.Api.Models.Dtos;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CliveKumara.CarwashManagement.Api.Controllers
 {
@@ -13,12 +17,16 @@ namespace CliveKumara.CarwashManagement.Api.Controllers
     public class CategoriesController : ControllerBase
     {
         private readonly CarWashManagementDbContext _dbContext;
+        private readonly IMapper mapper;
 
-        public CategoriesController(CarWashManagementDbContext dbContext)
+        public CategoriesController(CarWashManagementDbContext dbContext, IMapper mapper)
         {
             this._dbContext = dbContext;
+            this.mapper = mapper;
         }
 
-
+        [HttpGet]
+        public async Task<List<CategoryDto>> Get() => 
+            this.mapper.Map<List<CategoryDto>>(await this._dbContext.Categories.Select(o => o).ToListAsync());
     }
 }
